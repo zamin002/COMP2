bf7652fccc8bb733ff364e05fc3482d238f7cb66
zamin002 <148207180+zamin002@users.noreply.github.com>
2025-04-04 10:58:04 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AppProvider } from './src/context/AppContext'
+import AppNavigator from './src/navigation/AppNavigator'
+
+export default function App() {
+  return (
+    <AppProvider>
+      <AppNavigator />
+    </AppProvider>
+  )
+}
